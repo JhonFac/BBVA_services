d737dcfb866aa0a53639481c982c8953c13a04d1
unknown <maserati92@hotmail.com>
2023-01-11 17:28:51 -0500

    changue views and url

--- app/views.py
+++ app/views.py
@@ -8,5 +8,61 @@ class list(APIView):
 
     # listar listar
     def get(self, *args):
-        return Response({'mensaje':'listo'})
+        return Response({
+    "paymentMethods": [
+        "BankInvoice",
+        "DebitCard",
+        "Pix",
+        "CreditCard",
+        "Promissories",
+        "Privatelabels",
+        "Cobranded",
+        "Cash"
+            ]
+        })
+
+
+class Manifest(APIView):
+
+    # listar listar
+    def get(self, *args):
+        return Response({
+    "paymentMethods": [
+      {
+        "name": "Visa", 			
+        "allowsSplit": "onCapture" 
+      }, 
+      {
+         "name": "Pix", 			
+        "allowsSplit": "disabled"
+      },
+      {
+        "name": "MasterCard", 			
+        "allowsSplit": "onCapture" 
+      },
+      {
+        "name": "American Express", 			
+        "allowsSplit": "onCapture" 
+      },
+      {
+        "name": "BankInvoice", 			
+        "allowsSplit": "onAuthorize" 
+      },
+      {
+        "name": "Privatelabels", 			
+        "allowsSplit": "disabled" 
+      },
+      {
+        "name": "Promissories", 			
+        "allowsSplit": "disabled" 
+      }
+    ]
+   })
+
+
+
+class Payment(APIView):
+
+    def post(self, request):
+        return Response(data=request.data)
 
