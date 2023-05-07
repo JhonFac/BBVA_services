5ca9362dea8f3886524e7a530a8a5a58ae39056b
Jhon Bohorquez <jhonb@titamedia.com>
2023-05-07 13:33:15 -0500

    URLs change

--- app/urls.py
+++ app/urls.py
@@ -6,13 +6,14 @@ from app import views
 router = DefaultRouter()
 
 router.register(r"payment", views.PaymentViewSet, basename="Payment"),
-router.register(r"manifest", views.ManifestViewSet, basename="Manifest"),
+# router.register(r"manifest", views.ManifestViewSet, basename="Manifest"),
 router.register(r"transactions", views.GenerateTransactionViewSet, basename="Transactions"),
 router.register(r"create_transaction", views.CreateTransactionViewSet, basename="Create transaction"),
 
 
 urlpatterns = [
     path("get_transaction/", views.GetTransaction.as_view(), name="Get transaction"),
+    path("manifest/", views.ManifestViewSet.as_view(), name="manifest"),
     path("", include(router.urls))
 ]
     
