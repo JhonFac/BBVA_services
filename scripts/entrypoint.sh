b91373067c32c9a2fc68e5523dc96b7f7ec24761
Jhon Bohorquez <jhonb@titamedia.com>
2023-05-07 12:56:22 -0500

    entrypoint change

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,4 +6,5 @@ echo "Contenedor iniciado"
 echo "$(date): Ejecutando proceso"
 python manage.py makemigrations
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:$PORT
+# python manage.py runserver 0.0.0.0:$PORT
